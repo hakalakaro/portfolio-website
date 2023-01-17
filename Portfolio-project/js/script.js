5f7c7689b3ecd2c25e38cc83128c6baba3cd4168
Karo Hakala <83270123+hakalakaro@users.noreply.github.com>
2023-01-17 19:29:06 +0200

    Uploaded project to repository

---
+++ Portfolio-project/js/script.js
@@ -0,0 +1,7 @@
+/* ==== Typing animation ==== */
+var typed = new Typed(".typing", {
+    strings: ["", "Software Developer", "Web Developer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
